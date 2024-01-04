de2e0862202448ea1d0809ff58309651001a6d42
vorapat0881 <vorapat_g@cmu.ac.th>
2024-01-04 23:06:01 +0700

    lab11_1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+    int x = rand()%9;
+    if(x == 0) cout << "You will get A in this 261102.";
+    else if(x == 1) cout << "You will get B+ in this 261102.";
+    else if(x == 2) cout << "You will get B in this 261102.";
+    else if(x == 3) cout << "You will get C+ in this 261102.";
+    else if(x == 4) cout << "You will get C in this 261102.";
+    else if(x == 5) cout << "You will get D+ in this 261102.";
+    else if(x == 6) cout << "You will get D in this 261102.";
+    else if(x == 7) cout << "You will get F in this 261102.";
+    else cout << "You will get W in this 261102.";
+
+    return 0;
+
+}
